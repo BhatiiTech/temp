deb61ce3243d02c636d0d8b62824d222ae003fe5
bhatii <bhatiitechnolgy@gmail.com>
2024-09-17 19:41:50 +0530

    first backend commit

---
+++ src/main/java/com/deali/deali/DealiApplication.java
@@ -0,0 +1,13 @@
+package com.deali.deali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealiApplication.class, args);
+	}
+
+}
